ef32109ff30add45e4a9e9857bbf3f888fc24b06
Rahul suthar <sutharrahul119@gmail.com>
2024-04-07 16:36:15 +0530

    issue card create :tada:

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,23 @@
 import React from "react";
-import circledot from "../assets/dot.png";
 
 function Navbar() {
   return (
     <div className="px-8">
       <div className="navbar bg-base-200 border border-base-300 rounded-md">
-        <div className="flex-1">
-          <img className="h-5" src={circledot} alt="" />
-          <a className="btn btn-ghost text-xl">Issue</a>
+        <div className="flex-1 gap-2">
+          <svg
+            aria-hidden="true"
+            height="16"
+            viewBox="0 0 16 16"
+            version="1.1"
+            width="16"
+            data-view-component="true"
+            className="octicon octicon-issue-opened fill-white"
+          >
+            <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z"></path>
+            <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0ZM1.5 8a6.5 6.5 0 1 0 13 0 6.5 6.5 0 0 0-13 0Z"></path>
+          </svg>
+          <h2 className="text-white text-lg font-semibold">13 Issue</h2>
         </div>
       </div>
     </div>
