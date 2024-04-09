81f4cd399d7f0cc11eeffd6b7e8faf88d1abb9b2
Rahul suthar <sutharrahul119@gmail.com>
2024-04-10 00:24:34 +0530

    issue detail page created :fire:

--- src/IssueDetailPage.jsx
+++ src/IssueDetailPage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import RichTextEditor from "./components/RichTextEditor";
+import HeaderDetailPage from "./components/HeaderDetailPage";
 
 function IssueDetailPage() {
-  return <div>Boooooooom</div>;
+  return (
+    <div>
+      <HeaderDetailPage />
+      <RichTextEditor />
+    </div>
+  );
 }
 
 export default IssueDetailPage;
