b586f05ba37be4762fef249641b2f73e63ce5d1a
Rahul suthar <sutharrahul119@gmail.com>
2024-04-10 00:25:01 +0530

    avatar component create :boom:

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Avatar() {
+  return (
+    <div>
+      <div className="avatar">
+        <div className="w-16 rounded-full">
+          <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Avatar;
