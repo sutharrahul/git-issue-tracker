8566438e1dd22d29075ebaef23cb2756b44a8da7
Rahul suthar <sutharrahul119@gmail.com>
2024-04-11 14:36:46 +0530

    avatar component :evergreen_tree:

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Avatar() {
   return (
     <div>
-      <div className="avatar">
-        <div className="w-16 rounded-full">
+      <div className="avatar ">
+        <div className="w-10 rounded-full ">
           <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
         </div>
       </div>
