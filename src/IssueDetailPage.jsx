38b38fc065ff11a7fe6b7db78e7be29399c329f3
Rahul suthar <sutharrahul119@gmail.com>
2024-05-15 17:25:17 +0530

    update

--- src/IssueDetailPage.jsx
+++ src/IssueDetailPage.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import RichTextEditor from "./components/RichTextEditor";
 import HeaderDetailPage from "./components/HeaderDetailPage";
+import SideInfo from "./components/SideInfo";
 
 function IssueDetailPage() {
   return (
-    <div>
-      <HeaderDetailPage />
-      <RichTextEditor />
+    <div className="flex justify-between">
+      <div>
+        <HeaderDetailPage />
+        <RichTextEditor />
+      </div>
+      <div className="py-10 hidden lg:inline">
+        <SideInfo />
+      </div>
     </div>
   );
 }
